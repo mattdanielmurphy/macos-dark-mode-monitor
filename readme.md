2fbb9008402cbf27a6e0eabeb2905153bfcd90c8
Matthew Daniel Murphy <matt@mattmurphy.ca>
2021-02-28 17:33:06 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# macOS Dark Mode Monitor
+
+## Development
+
+- Run `yarn compile` to compile `index.js` to `/dist`
---
+++ scripts/compile.sh
@@ -0,0 +1,3 @@
+rm terminal-dark-mode
+echo "#!/usr/bin/env node" | cat - index.js > terminal-dark-mode
+chmod +x terminal-dark-mode
